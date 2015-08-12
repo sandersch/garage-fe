57110bf353bdb60e030a518fff217d878fbae6c9
Charlie Sanders <sanderscharlie@gmail.com>
2015-08-12 00:43:32 -0500

    poll for updates to door status

--- app/controllers/doors.js
+++ app/controllers/doors.js
@@ -2,7 +2,17 @@ import App from '../app';
 import Ember from 'ember';
 
 App.DoorsController = Ember.Controller.extend({
-  thing: "asdf"
+  init: function() {
+    this._super();
+    this.poll();
+  },
+
+  poll: function() {
+    Ember.run.later(this, function() {
+      this.get('model').store.findAll('door');
+      this.poll();
+    }, 5000);
+  }
 });
 
 export default App.DoorsController;
