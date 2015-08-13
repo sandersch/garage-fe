4a0b56928cb569291b47f2894ae491636b62316f
Charlie Sanders <sanderscharlie@gmail.com>
2015-08-12 23:00:58 -0500

    move status label class determination into component

--- app/components/garage-door.js
+++ app/components/garage-door.js
@@ -1,4 +1,14 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  statusClass: function() {
+    status = this.get('door.status')
+    if("open" == status) {
+      return 'btn-success';
+    } else if("closed" == status) {
+      return 'btn-danger';
+    } else {
+      return 'btn-warning';
+    }
+  }.property('door.status')
 });
--- app/models/door.js
+++ app/models/door.js
@@ -3,12 +3,4 @@ import DS from 'ember-data';
 
 export default App.Door = DS.Model.extend({
   status: DS.attr('string'),
-
-  isOpen: function() {
-    if(this.get('status') === 'open') {
-      return true;
-    } else {
-      return false;
-    }
-  }.property('status')
 });
