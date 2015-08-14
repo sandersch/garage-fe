26d9d05ce11d73f99a29d15061f8ca3624a99e70
Charlie Sanders <sanderscharlie@gmail.com>
2015-08-14 01:08:31 -0500

    fix jshint issues

--- app/components/garage-door.js
+++ app/components/garage-door.js
@@ -2,10 +2,10 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   statusClass: function() {
-    status = this.get('door.status')
-    if("open" == status) {
+    var status = this.get('door.status');
+    if("open" === status) {
       return 'btn-danger';
-    } else if("closed" == status) {
+    } else if("closed" === status) {
       return 'btn-success';
     } else {
       return 'btn-warning';
