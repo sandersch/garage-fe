e03d083bba050beec6bc985a601d32ce00b8b907
Charlie Sanders <sanderscharlie@gmail.com>
2015-08-14 14:53:47 -0500

    Revert "hack status when moving door"
    This reverts commit 140ac837cbd5072619b06f45e70516329d09874c.

--- app/models/door.js
+++ app/models/door.js
@@ -8,22 +8,11 @@ export default App.Door = DS.Model.extend({
     console.log('moving door');
     console.log(this);
     console.log(this.get('path'));
-    var path = 'doors/%@'.fmt(this.get('id'));
+    var path = 'door/%@'.fmt(this.get('id'));
     console.log("path = " + path);
-    var door = this
     $.ajax(path, { method: 'PUT', data: "{\"position\": \"open\"}", dataType: 'json', contentType:"application/json; charset=utf-8" }).then(function(resp) {
-      door.set('status', 'moving');
-      door.set('hasDirtyAttributes', false);
       console.log('door should be moving now');
       console.log(resp);
-    },function() { 
-      console.log('errback for move');
-      door.set('status', 'moving');
-      door.hasDirtyAttributes = false
-      Ember.run.later(function() {
-        door.rollback();
-        door.reload();
-      }, 1000);
     });
   }
 });
