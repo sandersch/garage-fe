050c97a7cf1efc193530a26c8070b5a9840c8d92
Charlie Sanders <sanderscharlie@gmail.com>
2015-08-14 14:55:03 -0500

    use more consistent route names

--- app/models/door.js
+++ app/models/door.js
@@ -8,7 +8,7 @@ export default App.Door = DS.Model.extend({
     console.log('moving door');
     console.log(this);
     console.log(this.get('path'));
-    var path = 'door/%@'.fmt(this.get('id'));
+    var path = 'doors/%@'.fmt(this.get('id'));
     console.log("path = " + path);
     $.ajax(path, { method: 'PUT', data: "{\"position\": \"open\"}", dataType: 'json', contentType:"application/json; charset=utf-8" }).then(function(resp) {
       console.log('door should be moving now');
